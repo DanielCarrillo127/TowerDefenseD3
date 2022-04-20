bfe07574d170e7f2f98560c1e069370e9471c12b
David Solano <solanoed@uninorte.edu.co>
2022-04-20 12:27:29 -0500

    improved some aspects

--- Assets/Scripts/Enemy_Actor.cs
+++ Assets/Scripts/Enemy_Actor.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Enemy_Actor : MonoBehaviour
 {
-    float speed = 2.5f;
+    public float speed = 2.5f;
     public Transform target;
     public Transform damagePoint;
 
